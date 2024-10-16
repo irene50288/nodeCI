cf36b8215b31f6da7e80eef2466dbfe38f2f9eb6
Iryna Koroliuk <iryna@Irynas-MacBook-Pro.local>
2024-10-16 12:44:26 -0700

    ci keys added

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI:
+    'mongodb+srv://koroliukiryna:JOYJuYKUHoLZfM3i@cluster0.6iacy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
